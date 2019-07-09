79eb6f7b5fbbaf6666da4fbaa7be65b9f0dbba9d
LASER-Yi <ly0007@yeah.net>
2019-07-09 22:03:28 +0800

    Initial Commit

---
+++ Apod-SwiftUI/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Apod-SwiftUI
+//
+//  Created by LiangYi on 2019/7/9.
+//  Copyright © 2019 LiangYi. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
